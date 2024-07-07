b5a8894d32ff82ba4de18510e2abcf3d11b36fc4
System Administrator <root@quantumtekhs-MacBook-Pro.local>
2024-07-07 07:14:22 -0400

    added contract addresses

---
+++ packages/hardhat/contracts/BasedKemonokakiTribute.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+/// @title based : kemonokaki tribute
+/// @author tekh.eth : owl wilderness silent watcher firekeeper bat shield frog
+/// @notice tribute : send based kemonokaki 
+/// @dev source : https://github.com/OwlWilderness/based-books/tree/book-one
+/// 
+contract BasedKemonokakiTribute {
+    string public Name = "based kemonokaki tribute";
+    string public Symbol = "bkt";
+
+//BASE
+
+    //KEMONOKAKI CONTRACTS
+    address public CONTRACT_KEMONO = 0xEe7D1B184be8185Adc7052635329152a4d0cdEfA   
+
+    //SANWEI DAIO
+    address public CONTRACT_3SNW = 0xC0cf442e851318EAB3d710b49ceA7D5315B6e967
+
+//ETH
+    ///SAN CONTRACTS
+    address public CONTRACT_SAN = 0x33333333333371718A3C2bB63E5F3b94C9bC13bE;
+    address public CONTRACT_SANAV = 0xD375e9D0Cbc12813683FFb0Ab9bf85fBF83aceaf;
+    address public CONTRACT_SANWEAR = 0xAE2Bc979178E97e0688384Aab00055E67bEa91ed;
+    address public CONTRACT_SANPASS = 0x512C8C57C97905134b12a8Ab8479b57a327AA6eC;
+    address public CONTRACT_SANGA = 0x05bAF9f1961E1ed1dac222A1f97c479441B31b34
+
+    //WE BRING CONTRACTS
+    address public CONTRACT_RADCATS = 0x3bFC3134645ebe0393F90d6a19BcB20bD732964F
+    address public CONTRACT_RADBROS = 0xABCDB5710B88f456fED1e99025379e2969F29610a
+
+    //PEPE 
+    address public CONTRACT_PEPE_BE = 0xbe042e9d09CB588331Ff911c2B46FD833A3E5bd6
+    address public CONTRACT_PEPE_69 = 0x6982508145454Ce325dDbE47a25d4ec3d2311933    
+
+    //ENA
+    address public CONTRACT_ENA = 0x57e114B691Db790C35207b2e685D4A43181e6061
+    
+    //PEOPLE
+    address public CONTRACT_PEOPLE =  0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71
+    
+}
