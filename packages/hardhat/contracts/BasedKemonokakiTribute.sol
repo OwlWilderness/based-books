54afe243a14cdb5cf611f865538204173333d9cb
owlwilderness.eth <98717833+OwlWilderness@users.noreply.github.com>
2024-07-14 21:54:39 -0400

    add trait method

--- packages/hardhat/contracts/BasedKemonokakiTribute.sol
+++ packages/hardhat/contracts/BasedKemonokakiTribute.sol
@@ -23,18 +23,62 @@ contract BasedKemonokakiTribute {
     ///METADATA/////////////////////////////////////////////////////////////////////////////////////////
     /////////////bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m////////////////////////////
     /////https://bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m.ipfs.dweb.link/<id>////////
-    /////EXAMPLE: https://bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m.ipfs.dweb.link/614
-    ////////////////////////////bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m/////////////
+    ///////EXAMPLE: https://bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m.ipfs.dweb.link/1
+    ////////////////////////////bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m/////////////"
     string public hKEMONO_Im = "bafybeidzu77mfoxlxo65cusy5j47cwgg6fdzq4mw7g4kqerkzcrwapkc5m";///////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     ///IMAGE////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a/////////////////
     /////////////////ipfs://bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a/<id>.png////////
-    /////////////////EXAMPLE: ipfs://bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a/614.png
+    ///////////////////EXAMPLE: ipfs://bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a/1.png
     ////////////////////////////bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a/////////////
     string public hKEMONO_Ii = "bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a";///////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////{                                                                                                             ////
+    ////    "name": "Kemonokaki #1",                                                                                  ////
+    ////    "description": "kemonokaki is a hand-drawn PFP collection inspired by kemonomimi & neo-chibi aesthetics.",////
+    ////    "image": "ipfs://bafybeidxclt74frtmiuiisfquphn2qdukz2cg63cr7bwyblp3mfwr5gk6a/1.png",                      ////
+    ////    "attributes": [                                                                                           ////
+    ////        {                                                                                                     ////
+    ////            "trait_type": "Race",                                                                             ////
+    ////            "value": "Special"                                                                                ////
+    ////        },                                                                                                    ////
+    ////        {                                                                                                     ////
+    ////            "trait_type": "Special Indicator",                                                                ////
+    ////            "value": "Genesis"                                                                                ////
+    ////        }                                                                                                     ////
+    ////    ]                                                                                                         ////
+    ////}                                                                                                             ////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    bytes16[] public keys_nft = ["name","description","image","attributes"]; //not sure if this works like this.
+    bytes16[] public keys_att = ["trait_type","value"]
+    bytes32[] public keys_trt = ["Race","Special Indicator"] 
+
+    mapping(bytes32 ==> bytes32) public attributes;
+
+    ///@dev - dynamically trait keys (keys_trt) array - add if the trait does not exist 
+    function addtrait(bytes32 memory key) public returns (bytes32[]){
+
+        bool found = false;
+        for (uint i = 0  i < keys_trt.length; i++){
+            if (keccak256(keys_trt[i])==keccak256(key)) {
+                found = true;
+                break;
+            }
+        }
+
+        if(!found){
+            keys_trt.push(key)
+        }
+
+        return keys_trt;
+    }
+    
 
 
 
+    
 }
